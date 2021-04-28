b6ee0a6c2b256504f98a9c6950608f2fc067f16e
vollborn <oliver.vollborn@dreid.de>
2021-04-28 12:50:08 +0200

    Dashboard

--- resources/js/components/dashboard/SourceCodeCard.vue
+++ resources/js/components/dashboard/SourceCodeCard.vue
@@ -1,3 +1,47 @@
 <template>
-  <div></div>
+  <v-card>
+    <dashboard-card-title
+      :title="$t('dashboard.cards.source.title')"
+      :subtitle="$t('dashboard.cards.source.subtitle')"
+    />
+    <v-card-text class="text-center pb-8">
+      <div>
+        <div>
+          <v-icon
+            size="60"
+            color="secondary"
+            class="mb-6"
+          >
+            fab fa-github
+          </v-icon>
+        </div>
+        <div>
+          {{ $t('dashboard.cards.source.line1') }}
+          <br/>{{ $t('dashboard.cards.source.line2') }}
+        </div>
+        <div>
+          <v-btn
+            class="mt-6"
+            color="primary"
+            @click="redirectToGithub"
+          >
+            {{ $t('dashboard.cards.source.button') }}
+          </v-btn>
+        </div>
+      </div>
+    </v-card-text>
+  </v-card>
 </template>
+
+<script>
+import DashboardCardTitle from "./DashboardCardTitle";
+
+export default {
+  components: {DashboardCardTitle},
+  methods: {
+    redirectToGithub() {
+      window.location.href = 'https://github.com/vollborn/dynamic-servers';
+    }
+  }
+}
+</script>
--- resources/js/components/dashboard/SupportCard.vue
+++ resources/js/components/dashboard/SupportCard.vue
@@ -1,3 +1,42 @@
 <template>
-  <div></div>
+  <v-card>
+    <dashboard-card-title
+      :title="$t('dashboard.cards.support.title')"
+      :subtitle="$t('dashboard.cards.support.subtitle')"
+    />
+    <v-card-text class="text-center pb-8">
+      <div>
+        <div>
+          <v-icon
+            size="60"
+            color="secondary"
+            class="mb-6"
+          >
+            fa-question-circle
+          </v-icon>
+        </div>
+        <div>
+          {{ $t('dashboard.cards.support.line1') }}
+          <br/>{{ $t('dashboard.cards.support.line2') }}
+        </div>
+        <div>
+          <v-btn
+            class="mt-6"
+            color="primary"
+            @click="pushRouteTo({name : 'support'})"
+          >
+            {{ $t('dashboard.cards.support.button') }}
+          </v-btn>
+        </div>
+      </div>
+    </v-card-text>
+  </v-card>
 </template>
+
+<script>
+import DashboardCardTitle from "./DashboardCardTitle";
+
+export default {
+  components: {DashboardCardTitle}
+}
+</script>
